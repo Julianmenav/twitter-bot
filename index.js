6098f7168300a4e60cafe8bf88d6ce914d27d0be
Julianmenav <julianmenav2@gmail.com>
2022-04-27 21:20:01 +0200

    Update: Ahora si una cuenta no ha jugado revisa la cuenta siguiente del objetivo

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const { tweet } = require('./twitter-api-functions')
 const { getSummonerInfoByName, getMatchHistoryByPuuid } = require('./lol-api-functions')
 const { accounts } = require('./accountsList')
 const HOURS = 12
+const OBJETIVE = "lupiiiix"
 
-async function lolTweet(twitterName) {
+async function lolTweet(twitterName, acc) {
 
-  const summonerName = accounts[twitterName][0].name
+  const summonerName = accounts[twitterName][acc].name
   const tierData = await getSummonerInfoByName(summonerName)
   const { tier, rank, leaguePoints, puuid } = tierData
 
@@ -28,13 +29,20 @@ Cuenta: ${summonerName}
 ${tier} ${rank}   ${leaguePoints}LPs  
 `
 
+  //En caso de que no haya suficientes partidas, repite el proceso con la siguiente cuenta del usuario en la lista. usando recursividad.
   if (totalGames > 3){
-    tweet(text).catch(e => console.error(e))
+    await tweet(text).catch(e => console.error(e))
   } else {
-    tweet(`@${twitterName} no ha jugado lo suficiente en las últimas ${HOURS} horas bro...`).catch(e => console.error(e))
+    if (acc < accounts[twitterName].length - 1){
+      console.log(`Buscando en la siguiente cuenta de ${OBJETIVE}...`)
+      lolTweet(OBJETIVE, acc + 1)
+    } else {
+      console.log("No se encontraron partidas.")
+      await tweet(`@${twitterName} no ha jugado lo suficiente en las últimas ${HOURS} horas bro...`).catch(e => console.error(e))
+    }
   }
 }
 
-lolTweet("P0o_frek");
+lolTweet(OBJETIVE, 0);
 
 
