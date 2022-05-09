f42037d0bd1935c77f3a8d8a53c2d88e540eaaec
Julianmenav <julianmenav2@gmail.com>
2022-05-10 01:40:59 +0200

    New function: Tweet about random player of the list

--- lol-tweet.js
+++ lol-tweet.js
@@ -34,7 +34,7 @@ async function lolTweet(twitterName, acc, inReplyTo, inReplyToId) {
   //SI NO HAY SUFICIENTES PARTIDAS:
   if (totalGames < 3){
     //1. PASA A MIRAR LA SIGUIENTE CUENTA DEL JUGADOR (SI EXISTE)
-    if (acc < accounts[twitterName].length - 1){
+    if (acc < summoner.length - 1){
       console.log(`Buscando en la siguiente cuenta de ${OBJETIVE}...`)
       return lolTweet(OBJETIVE, acc + 1)
     }
@@ -44,7 +44,7 @@ async function lolTweet(twitterName, acc, inReplyTo, inReplyToId) {
     return await tweet(noGamesText, inReplyTo, inReplyToId).catch(e => console.error(e))
   } 
   //3. EN ESTE CASO SOLO SE SEGUIRÁ CON EL PROGRAMA SI LA ÚLTIMA PARTIDA HA SIDO HACE POCO +40 min, - 4 horas
-  let lastGameTime = matchData[0].timestamp
+  //let lastGameTime = matchData[0].timestamp
   //Guard clause. If the game is not between the time range, end the program.
   // if( !(lastGameTime < rightNowMS - 3600 * 40 && lastGameTime > rightNowMS - 3600000 * 4)){
   //   return console.log("Las última partida no es del todo reciente.")
@@ -73,6 +73,71 @@ ${tier} ${rank}  ${leaguePoints}LPs
   return await tweet(text, inReplyTo, inReplyToId).catch(e => console.error(e))
 }
 
+
+async function lolTweetX(twitterName, acc, inReplyTo, inReplyToId) {
+  console.log(`Buscando partidas para ${twitterName}...`)
+  const summoner = getParameterCaseInsensitive(accounts, twitterName)
+  //Si el nombre no está en nuestra lista descartamos.
+  if (!summoner){
+    return console.log("No está en la lista.")
+    //return await tweet(" No le sabe 😔", inReplyTo, inReplyToId).catch(e => console.error(e))
+  }
+  const summonerPuuid = summoner[acc].puuid
+  const tierData = await getSummonerInfoByPuuid(summonerPuuid)
+  const { tier, rank, leaguePoints, name} = tierData
+  
+  //Epoch timestamp in seconds
+  const rightNowMS = new Date().getTime()
+  const timestampLimit = Math.floor((rightNowMS - HOURS * 3600000) / 1000 )
+  const matchData = await getMatchHistoryByPuuid(summonerPuuid, timestampLimit)
+
+  const totalGames = matchData.length
+  const wins = matchData.filter(el => el.win).length
+  const loses = totalGames - wins
+  //3 OPCIONES. 
+  //SI NO HAY SUFICIENTES PARTIDAS:
+  if (totalGames < 3){
+    //1. PASA A MIRAR LA SIGUIENTE CUENTA DEL JUGADOR (SI EXISTE)
+    if (acc < accounts[twitterName].length - 1){
+      console.log(`Buscando en la siguiente cuenta de ${OBJETIVE}...`)
+      return lolTweet(OBJETIVE, acc + 1)
+    }
+    //2. UNA VEZ SE HAN MIRADO TODAS Y NO HAY PARTIDAS SUFICIENTES:
+    return console.log("No se encontraron partidas.")
+    // const noGamesText = `.@${twitterName} no ha jugado lo suficiente en las últimas ${HOURS} horas...`
+    // return await tweet(noGamesText, inReplyTo, inReplyToId).catch(e => console.error(e))
+  } 
+  //3. EN ESTE CASO SOLO SE SEGUIRÁ CON EL PROGRAMA SI LA ÚLTIMA PARTIDA HA SIDO HACE POCO (desde hace 1 hora ~ hace 5 horas)
+  let lastGameTime = matchData[0].timestamp
+  //Guard clause. If the game is not between the time range, end the program.
+  if( !(lastGameTime < rightNowMS - 3600 * 60 && lastGameTime > rightNowMS - 3600000 * 5)){
+    return console.log("Las última partida no es del todo reciente.")
+  }
+
+
+  console.log("Se encontraron partidas.")
+  console.log("Buscando los lps...")
+  const lpData = await lpScraper(name, totalGames)
+  
+  const makeSense = tester(matchData, lpData.order) //true or false
+  const lpText = `${lpData.lp > 0 ? "+" : ""}${lpData.lp} Lps 
+  [${lpData.order}]`
+  const text =
+  `
+  ${getSentence(totalGames, wins, twitterName)}
+  
+Cuenta: ${name}
+En las últimas ${HOURS} horas:
+${wins}W - ${loses}L
+${ makeSense && !isNaN(lpData.lp) ? lpText : "" }
+
+${tier} ${rank}  ${leaguePoints}LPs  
+`
+
+  return await tweet(text, inReplyTo, inReplyToId).catch(e => console.error(e))
+}
+
 module.exports = {
-  lolTweet
+  lolTweet,
+  lolTweetX
 }
