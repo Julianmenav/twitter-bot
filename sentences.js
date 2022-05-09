c777cd112e13405990d2db79399948c452195b9f
Julianmenav <julianmenav2@gmail.com>
2022-05-10 01:46:03 +0200

    Updated sentences

--- sentences.js
+++ sentences.js
@@ -1,17 +1,17 @@
 
 const getSentence = (totalGames, wins, twitterName) => {
   const winRatio = wins / totalGames
-  let mention = `@ ${twitterName}`
+  let mention = `@${twitterName}`
   let sentence = "";
   //Based on Win ratio: 5 GRADOS: 0-0.25; 0.25-0.45; 0.45-0.55; 0.55-0.75, 0.75-1
   //Based on totalGames:   >6
   if (totalGames > 5) {
     switch (true) {
       case (winRatio < 0.25):
-        sentence = [`Señoras y señores, ${mention} ES la pochiqueue`, `.${mention} no debería haber jugado hoy`, `x9 ${mention} for int please`]
+        sentence = [`Señoras y señores, ${mention} ES la pochiQ`, `.${mention} no debería haber jugado hoy`, `x9 ${mention} for int please`]
         break;
       case (winRatio < 0.45):
-        sentence = [`Parece que ${mention} va en silla de ruedas`, `Mal día para ${mention}`, `.${mention} esta troleando`]
+        sentence = [`Mal día para ${mention}`, `.${mention} esta troleando`]
         break;
       case (winRatio < 0.6):
         sentence = [`.${mention} stucked?`, `.${mention} va a por el 50% winrate`]
@@ -20,13 +20,13 @@ const getSentence = (totalGames, wins, twitterName) => {
         sentence = [`A ${mention} no le va mal!`, `.${mention} tiene manos`]
         break;
       default:
-        sentence = [`.${mention} SE LA ESTA SHACANDO`, `.${mention} modo Faker`, `.${mention} va volando`]
+        sentence = [`.${mention} se la esta shacando`, `.${mention} modo Faker`, `.${mention} va volando`]
         break;
     }
   } else {
     switch (true) {
       case (winRatio < 0.25):
-        sentence = [`A ${mention} no le estan saliendo las cosas`, `Parece que ${mention} va en silla de ruedas`]
+        sentence = [`A ${mention} no le estan saliendo las cosas`]
         break;
       case (winRatio < 0.45):
         sentence = [`Mal día para ${mention}`, `.${mention} está stucked`]
