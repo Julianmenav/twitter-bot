d9ff4d0127ca08731068bf7dddeb5f4f7b02c5b7
Julianmenav <julianmenav2@gmail.com>
2022-05-10 00:32:47 +0200

    refactor sentences

--- sentences.js
+++ sentences.js
@@ -1,43 +1,44 @@
 
 const getSentence = (totalGames, wins, twitterName) => {
   const winRatio = wins / totalGames
+  let mention = `@ ${twitterName}`
   let sentence = "";
   //Based on Win ratio: 5 GRADOS: 0-0.25; 0.25-0.45; 0.45-0.55; 0.55-0.75, 0.75-1
   //Based on totalGames:   >6
   if (totalGames > 5) {
     switch (true) {
       case (winRatio < 0.25):
-        sentence = [`Señoras y señores, @${twitterName} ES la pochiqueue`, `.@${twitterName} no debería haber jugado hoy`, `x9 @${twitterName} for int please`]
+        sentence = [`Señoras y señores, ${mention} ES la pochiqueue`, `.${mention} no debería haber jugado hoy`, `x9 ${mention} for int please`]
         break;
       case (winRatio < 0.45):
-        sentence = [`Parece que @${twitterName} va en silla de ruedas`, `Mal día para @${twitterName}`, `.@${twitterName} esta troleando`]
+        sentence = [`Parece que ${mention} va en silla de ruedas`, `Mal día para ${mention}`, `.${mention} esta troleando`]
         break;
       case (winRatio < 0.6):
-        sentence = [`.@${twitterName} stucked?`, `.@${twitterName} va a por el 50% winrate`]
+        sentence = [`.${mention} stucked?`, `.${mention} va a por el 50% winrate`]
         break;
       case (winRatio < 0.76):
-        sentence = [`A @${twitterName} no le va mal!`, `.@${twitterName} tiene manos`]
+        sentence = [`A ${mention} no le va mal!`, `.${mention} tiene manos`]
         break;
       default:
-        sentence = [`.@${twitterName} SE LA ESTA SHACANDO`, `.@${twitterName} modo Faker`, `.@${twitterName} va volando`]
+        sentence = [`.${mention} SE LA ESTA SHACANDO`, `.${mention} modo Faker`, `.${mention} va volando`]
         break;
     }
   } else {
     switch (true) {
       case (winRatio < 0.25):
-        sentence = [`A @${twitterName} no le estan saliendo las cosas`, `Parece que @${twitterName} va en silla de ruedas`]
+        sentence = [`A ${mention} no le estan saliendo las cosas`, `Parece que ${mention} va en silla de ruedas`]
         break;
       case (winRatio < 0.45):
-        sentence = [`Mal día para @${twitterName}`, `.@${twitterName} está stucked`]
+        sentence = [`Mal día para ${mention}`, `.${mention} está stucked`]
         break;
       case (winRatio < 0.67):
-        sentence = [`.@${twitterName} está en su pick elo`]
+        sentence = [`.${mention} está en su pick elo`]
         break;
       case (winRatio < 0.76):
-        sentence = [`A @${twitterName} no le va mal`, `.@${twitterName} progresa adecuadamente`]
+        sentence = [`A ${mention} no le va mal`, `.${mention} progresa adecuadamente`]
         break;
       default:
-        sentence = [`.@${twitterName} está ganando partidas`, `.@${twitterName} está en la winners queue`]
+        sentence = [`.${mention} está ganando partidas`, `.${mention} está en la winners queue`]
         break;
     }
   }
